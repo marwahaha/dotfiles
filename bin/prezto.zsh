1f97e26385db57985efb09e2ba4289248e0e720a
Christian Bundy <me@christianbundy.com>
2014-10-05 23:13:22 +0000

    Add NeoBundle, chowning, and .tmux.conf

--- bin/prezto.zsh
+++ bin/prezto.zsh
@@ -11,6 +11,9 @@ if [[ (! -d $VAGRANT_HOME"/.zprezto")]]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "$VAGRANT_HOME/.zprezto" >> /vagrant/provision.log 2>&1
 fi
 
+echo "Downloading NeoBundle..."
+git clone https://github.com/Shougo/neobundle.vim $VAGRANT_HOME/.vim/bundle/neobundle.vim
+
 # Default Prezto dotfiles
 echo "Symlinking Prezto dotfiles..."
 for rcfile in $VAGRANT_HOME/.zprezto/runcoms/^README.md(.N); do
@@ -23,7 +26,11 @@ for rcfile in $VAGRANT_SHARE/dotfiles/*; do
   ln -sf "$rcfile" "$VAGRANT_HOME/.${rcfile:t}" >> /vagrant/provision.log 2>&1
 done
 
+
 echo "Setting default shell..."
 chsh -s /bin/zsh vagrant >> /vagrant/provision.log 2>&1
+
+echo "Chowning ~ to the \"vagrant\" user..."
+sudo chown -R vagrant .
 
 echo "Done!"
