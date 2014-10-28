973d624a32a15c16ee0f0432b5c09eccbb717ccb
Christian Bundy <me@christianbundy.com>
2014-10-28 15:22:27 -0700

    Update vagrant.zsh with working code

--- bin/vagrant.zsh
+++ bin/vagrant.zsh
@@ -1,43 +1,33 @@
 #!/usr/bin/env zsh
 
+SHARE="/vagrant"
+
+echo "==> Installing Prezto..."
+git clone --recursive -q https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
-
-sudo -u vagrant -s zsh
-
-VAGRANT_SHARE="/vagrant"
-VAGRANT_HOME="/home/vagrant"
-HOME="/home/vagrant/"
-
-echo "==> Downloading Prezto..."
-if [[ (! -d $VAGRANT_HOME"/.zprezto")]]; then
-  git clone --recursive -q https://github.com/sorin-ionescu/prezto.git "$VAGRANT_HOME/.zprezto"
-fi
-
-echo "==> Downloading NeoBundle..."
-git clone -q https://github.com/Shougo/neobundle.vim "$VAGRANT_HOME/.vim/bundle/neobundle.vim"
-
-# Default Prezto dotfiles
-echo "==> Symlinking Prezto dotfiles..."
-for rcfile in $VAGRANT_HOME/.zprezto/runcoms/^README.md(.N); do
-  ln -sf "$rcfile" "$VAGRANT_HOME/.${rcfile:t}"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# Custom dotfiles
-echo "==> Symlinking custom dotfiles..."
-for rcfile in $VAGRANT_SHARE/dotfiles/*; do
-  ln -sf "$rcfile" "$VAGRANT_HOME/.${rcfile:t}"
-done
+echo "==> Installing NeoBundle..."
+git clone -q https://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
+
+echo "==> Installing NeoBundle plugins..."
+"$HOME/.vim/bundle/neobundle.vim/bin/neoinstall"
 
 echo "==> Installing NVM..."
 curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.17.3/install.sh | bash
+nvm install $(cat "$SHARE/dotfiles/nvmrc")
 
 echo "==> Installing RVM..."
 curl -sSL https://get.rvm.io | bash -s stable
 
-echo "Installing Vim plugins..."
-vim +NeoBundleInstall +qall
+echo "==> Installing Meteor..."
+curl -sSL https://install.meteor.com | /bin/sh
 
-echo "==> Setting default shell..."
-chsh -s /bin/zsh
+echo "==> Symlinking custom dotfiles..."
+for rcfile in $SHARE/dotfiles/*; do
+  ln -sf "$rcfile" "$HOME/.${rcfile:t}"
+done
 
 echo "==> Done!"
