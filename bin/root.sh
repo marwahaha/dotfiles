f1ba4b5f00507e25d59a26fa48919f31b5c846a0
Christian Bundy <me@christianbundy.com>
2014-10-28 16:48:47 -0700

    Make vagrant provision on first login

--- bin/root.sh
+++ bin/root.sh
@@ -5,5 +5,8 @@ apt-get update
 echo "==> Installing packages..."
 apt-get install -y zsh git fortune cowsay
 
+# Make vagrant run vagrant.zsh on first login
+echo "/vagrant/bin/vagrant.zsh" > /home/vagrant/.zshrc
+
 # Change default shell for vagrant
 chsh -s $(which zsh) vagrant
