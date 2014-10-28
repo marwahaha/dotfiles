58240215ada37c8a1c3e6b3d53573957d1a515fc
Christian Bundy <me@christianbundy.com>
2014-10-28 16:52:06 -0700

    Install ruby and reorder vagrant.zsh

--- bin/vagrant.zsh
+++ bin/vagrant.zsh
@@ -12,26 +12,29 @@ done
 echo "==> Installing Vundle..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "==> Installing NVM..."
-curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.17.3/install.sh | bash
-
-echo "==> Installing RVM..."
-curl -sSL https://get.rvm.io | bash -s stable
-
-echo "==> Installing Meteor..."
-curl -sSL https://install.meteor.com | /bin/sh
-
 echo "==> Symlinking custom dotfiles..."
 for rcfile in $SHARE/dotfiles/*; do
   ln -sf "$rcfile" "$HOME/.${rcfile:t}"
 done
 
-echo "==> Install Base16 Shell"
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+echo "==> Installing NVM..."
+curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.17.3/install.sh | bash
 
 echo  "==> Installing Node..."
 source ~/.nvm/nvm.sh
 nvm install $(cat "$SHARE/dotfiles/nvmrc")
+
+echo "==> Installing RVM..."
+curl -sSL https://get.rvm.io | bash -s stable
+
+echo "==> Installing Ruby..."
+rvm install ruby --latest
+
+echo "==> Installing Meteor..."
+curl -sSL https://install.meteor.com | /bin/sh
+
+echo "==> Install Base16 Shell"
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 echo "==> Done!"
 (tmux attach 2>&1 > /dev/null || tmux 2>&1 > /dev/null) && exit
