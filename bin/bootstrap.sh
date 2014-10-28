0278c8de1469c7b55d8532a8df6ae857f1964a74
Christian Bundy <me@christianbundy.com>
2014-10-28 14:29:35 -0700

    Remove logging from bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-# Clear logfile
-> /vagrant/provision.log
+echo "==> Updating package manager..."
+apt-get update
 
-echo "Updating package manager..."
-apt-get update > /vagrant/provision.log 2>&1
+echo "==> Installing Git and Zsh..."
+apt-get install -y zsh git fortune cowsay
 
-echo "Installing Git and Zsh..."
-apt-get install -y zsh git fortune cowsay >> /vagrant/provision.log 2>&1
+echo "==> Installing Meteor..."
+curl -s https://install.meteor.com/ | sh
 
-echo "Installing Meteor..."
-curl -s https://install.meteor.com/ | sh >> /vagrant/provision.log 2>&1
 # Configure ZSH
 /vagrant/bin/prezto.zsh
