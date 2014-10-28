1dd402cfd5752cc9c3aad3074d5f1836c922e8e8
Christian Bundy <me@christianbundy.com>
2014-10-28 15:14:27 -0700

    Change default shell for vagrant

--- bin/root.sh
+++ bin/root.sh
@@ -5,5 +5,8 @@ apt-get update
 echo "==> Installing packages..."
 apt-get install -y zsh git fortune cowsay
 
+# Change default shell for vagrant
+chsh -s $(where zsh | head -n 1) vagrant
+
 # Run vagrant.zsh as vagrant
 runuser -l vagrant -c '/vagrant/bin/vagrant.zsh'
