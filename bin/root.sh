7ad1ff039cdfc96a721ca7a44f28f4918242cffc
Christian Bundy <me@christianbundy.com>
2014-10-28 14:58:04 -0700

    Remove Meteor and run vagrant.zsh as vagrant

--- bin/root.sh
+++ bin/root.sh
@@ -5,8 +5,5 @@ apt-get update
 echo "==> Installing Git and Zsh..."
 apt-get install -y zsh git fortune cowsay
 
-echo "==> Installing Meteor..."
-curl -s https://install.meteor.com/ | sh
-
-# Configure ZSH
-/vagrant/bin/prezto.zsh
+# Run commands as vagrant
+sudo -u vagrant /vagrant/bin/vagrant.zs
